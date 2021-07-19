651bbb09687515af70fca39b0629fa57e3b4fe8b
dongju.park <dongju.park@navercorp.com>
2021-07-20 00:40:21 +0900

    Initial base project

---
+++ src/kobacktranslation/__init__.py
@@ -0,0 +1,10 @@
+__title__ = "KoBackTranslation"
+__version__ = "0.0.0"
+
+__author__ = "Dongju Park"
+__email__ = "toriving@gmail.com"
+__url__ = "http://toriving.github.io"
+
+__summary__ = "Korean Back-Translation"
+__license__ = "MIT License"
+__copyright__ = "Copyright 2021 {}".format(__author__)
